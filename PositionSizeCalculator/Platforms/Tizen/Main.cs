8b2173caf0de2ffc2a1108b27a66297b2dd18f70
Mattias Westman <mattiaswestman@outlook.com>
2026-04-16 16:36:17 +0200

    Renamed namespace.

--- PositionSizeCalculator/Platforms/Tizen/Main.cs
+++ PositionSizeCalculator/Platforms/Tizen/Main.cs
@@ -2,7 +2,7 @@ using System;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 
-namespace Position_Size_Calculator
+namespace PositionSizeCalculator
 {
     internal class Program : MauiApplication
     {
